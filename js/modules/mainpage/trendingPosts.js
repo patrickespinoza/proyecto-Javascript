01a4be691d82f90143dad2edbaf4c0b5cc41a89d
luis rodriguez <soyriku@gmail.com>
2024-05-08 01:55:47 -0500

    FIX/ Improved printTrendingPosts function

--- js/modules/mainpage/trendingPosts.js
+++ js/modules/mainpage/trendingPosts.js
@@ -7,16 +7,36 @@ const getTopPosts = (posts) => {
     }
     return trendingPosts;
 };
+
+const getKeyTopPosts = (posts) => {
+    let postKeys = Object.keys(posts);
+    let trendingposts = postKeys.reduce((acc, key) => {
+        if (posts[key].rating >= 4) {
+            posts[key].key = key;
+            acc[key] = posts[key];
+        }
+        return acc;
+    }, {});
+
+    return trendingposts;
+};
+
 const printTrendingPosts = (posts, containerId) => {
+    
     let trendingPostContainer = document.getElementById(containerId);
-    let trendingPosts = getTopPosts(posts)
+    let trendingPosts = getKeyTopPosts(posts);
+    
+    
     for (let post in trendingPosts) {
+        
+        const keyURL = `post.html?key=${trendingPosts[post].key}`
         let postElement = document.createElement("a");
         postElement.classList.add("btn", "btn-light", "text-start");
-        postElement.setAttribute("href", "#");
+        postElement.setAttribute("href", keyURL);
         postElement.textContent = posts[post].title;
         trendingPostContainer.appendChild(postElement);
     }
+// se puede hacer con forEach o map pero usaremos esta funcion por variedad
 
 }
 
